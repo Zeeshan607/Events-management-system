5487a86a8da871835a85103159d70a3e534ec6fb
Muhammad Zeeshan <zeeshanawan1998@gmail.com>
2024-06-03 18:09:33 +0500

    fixed events seeders id error

--- resources/views/admin/layouts/app.blade.php
+++ resources/views/admin/layouts/app.blade.php
@@ -177,7 +177,7 @@
                             <span class="text-dark">{{\Auth::guard("admin")->user()->name}}</span>
                         </a>
                         <div class="dropdown-menu dropdown-menu-end">
-                            <a class="dropdown-item" href="pages-profile.html"><i class="align-middle me-1" data-feather="user"></i> Profile</a>
+                            <a class="dropdown-item" href="#"><i class="align-middle me-1" data-feather="user"></i> Profile</a>
 {{--                            <a class="dropdown-item" href="#"><i class="align-middle me-1" data-feather="pie-chart"></i> Analytics</a>--}}
                             <div class="dropdown-divider"></div>
 {{--                            <a class="dropdown-item" href="index.html"><i class="align-middle me-1" data-feather="settings"></i> Settings & Privacy</a>--}}
