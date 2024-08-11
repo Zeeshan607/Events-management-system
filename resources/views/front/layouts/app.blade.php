e5ff1284030a0a78e20ba8e0cbb567530ca1d275
Muhammad Zeeshan <zeeshanawan1998@gmail.com>
2024-08-11 17:45:03 +0500

    added gtags scripts

--- resources/views/front/layouts/app.blade.php
+++ resources/views/front/layouts/app.blade.php
@@ -14,13 +14,35 @@
     <link rel="stylesheet" href="{{asset('web/assets/plugins/pretty-checkbox-3.0/pretty-checkbox.min.css')}}">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/pretty-checkbox@3.0/dist/pretty-checkbox.min.css">
 
+    <script async rel="preconnect" src="https://www.googletagmanager.com/gtag/js?id=G-LD1F4DVE0S" data-pagespeed-no-defer crossorigin></script>
+    <!-- Google tag (gtag.js) -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-LD1F4DVE0S"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-LD1F4DVE0S');
+    </script>
+    <!-- Google Tag Manager -->
+    <script rel="preconnect" >(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-P277PVX2');</script>
+    <!-- End Google Tag Manager -->
+
     @if(in_array(Route::currentRouteName(),['chat.index']))
     @vite('resources/js/user_chat/user_chat_app.js')
     @endif
 </head>
 <body>
 
-
+<div class="project-static-banner">
+    <span><b>Guide:</b>Project's basic documentation and instruction to setup are
+        available in readme.md file of git repo, which can be visited at
+        <a href="https://github.com/Zeeshan607/Events-management-system/blob/master/README.md" target="_blank">GITHUB</a></span>
+</div>
 
 @if(!request()->is("/"))
 
